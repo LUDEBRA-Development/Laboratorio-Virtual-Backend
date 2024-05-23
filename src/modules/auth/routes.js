5ac250ed4964580007413b079eddff7a9f256336
luis301311 <luisalbertovega@unicear.edu.co>
2024-05-22 22:27:49 -0500

    post

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const response = require('../../red/response')
 const controller = require('./controller');
 
-routes.get('/login', async (req, res, next)=>{
+routes.post('/login', async (req, res, next)=>{
     try{
         const token = await controller.login(req.body.Email, req.body.Password);
         response.success(req, res, token, 200);
