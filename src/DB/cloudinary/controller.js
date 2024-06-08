1c71f8532e2311ac0b49be11ab2ee796b0fd6570
luis301311 <luisalbertovega@unicear.edu.co>
2024-06-07 23:23:30 -0500

    lates files

--- src/DB/cloudinary/controller.js
+++ src/DB/cloudinary/controller.js
@@ -12,23 +12,15 @@ async function add(file,folder){
 async function update(file, folder, existingPublicId) {
     try {
         let result;
-
-        // Verificar si existe un ID público existente
         if (existingPublicId) {
-            console.log("Actualizando archivo existente...");
             result = await cloudinary.uploader.upload(file.tempFilePath, {
                 public_id: existingPublicId,
                 overwrite: true,
                 folder: folder
             });
         } else {
-            console.log("Subiendo nuevo archivo...");
             result = await cloudinary.uploader.upload(file.tempFilePath, { folder: folder });
         }
-
-        console.log("URL del archivo actualizado:", result.secure_url);
-        console.log("Public ID del archivo actualizado:", result.public_id);
-
         return {
             success: true,
             url: result.secure_url,
@@ -38,7 +30,7 @@ async function update(file, folder, existingPublicId) {
         console.error("Error al actualizar el archivo:", error);
         return {
             success: false,
-            error: error.message // Puedes ajustar el manejo de errores según tus necesidades
+            error: error.message
         };
     }
 }
